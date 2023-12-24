b4dd65e12b8a96ede7bc23150dccd860e5409937
prxshetty <prxshetty@gmail.com>
2023-12-23 23:53:50 -0500

    added app.py that runs a basic web page based on streamlit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.sidebar.title("Whatsapp Chat Analyzer")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
---
+++ preprocessor.py
@@ -0,0 +1,32 @@
+def preprocess(data):
+    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    messages = re.split(pattern, data)[1:]
+    dates = re.findall(pattern, data)
+    # convert message_date type
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%m/%d/%y, %H:%M - ')
+    # chats where in m.d.y format, so i had to alter the code language as well,
+    # another approach to fix that was maybe use the errors='coerce'  to declare those values
+    # NaT which cause problems while parsing but would reduce the rows
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+    users = []
+    messages = []
+    for message in df['user_message']:
+        entry = re.split('([\w\W]+?):\s', message)
+        if entry[1:]:  # user name
+            users.append(entry[1])
+            messages.append(" ".join(entry[2:]))
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
+    df['user'] = users
+    df['messsage'] = messages
+    df.drop(columns=['user_message'], inplace=True)
+
+    df['year'] = df['date'].dt.year
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+
+    return df
