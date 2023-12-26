4c48e0d22914e943f1d43810f18f7e1a2f90fd0e
Pranam Shetty <pranamshetty@Pranams-MacBook-Pro.local>
2023-12-26 14:34:40 -0500

    make it better

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import helper
 import preprocessor
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.sidebar.title("Whatsapp Chat Analyzer")
 
@@ -9,11 +10,11 @@ uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
-    data=bytes_data.decode("utf_8")
-    df=preprocessor.preprocess(data)
-    st.dataframe(df)
+    data = bytes_data.decode("utf_8")
+    df = preprocessor.preprocess(data)
+
     # fetches unique users
-    user_list=df['user'].unique().tolist()
+    user_list = df['user'].unique().tolist()
     user_list.remove('group_notification')
     user_list.sort()
     user_list.insert(0,'Overall')
@@ -21,10 +22,9 @@ if uploaded_file is not None:
     selected_user = st.sidebar.selectbox("Show analysis wrt",user_list)
 
     if st.sidebar.button("Show Analysis"):
-        num_messages, words, num_media_messages, no_of_links= helper.fetch_stats(selected_user,df)
-
+        num_messages, words, num_media_messages, no_of_links = helper.fetch_stats(selected_user,df)
+        st.title("Top Statistics")
         col1, col2, col3, col4 = st.columns(4)
-
         with col1:
             st.header("Total Messages")
             st.title(num_messages)
@@ -37,6 +37,48 @@ if uploaded_file is not None:
         with col4:
             st.header("Total Links")
             st.title(no_of_links)
+
+        # monthly timeline
+        st.title("Monthly Timeline")
+        timeline = helper.monthly_timeline(selected_user,df)
+        fig, ax = plt.subplots()
+        ax.plot(timeline['time'], timeline['message'],color='green')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        # daily timeline
+        st.title("Daily Timeline")
+        daily_timeline = helper.daily_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.plot(daily_timeline['only_date'], daily_timeline['message'], color='black')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        # activity monitor
+        st.title("Activity Map")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.header("Most Busy Day")
+            busy_day = helper.week_activity_map(selected_user,df)
+            fig, ax = plt.subplots()
+            ax.bar(busy_day.index, busy_day.values)
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+        with col2:
+            st.header("Most Busy Month")
+            busy_month = helper.month_activity_map(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.bar(busy_month.index, busy_month.values,color='purple')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+
+        # Heatmap Weekly
+        st.title("Weekly Activity Map")
+        user_heatmap = helper.activity_heatmap(selected_user, df)
+        fig, ax = plt.subplots()
+        ax = sns.heatmap(user_heatmap)
+        st.pyplot(fig)
+
 
         # finding the busiest user in the group(Group level)
         if selected_user == 'Overall':
@@ -73,5 +115,15 @@ if uploaded_file is not None:
         st.pyplot(fig)
 
         # emoji analysis
+        emoji_df = helper.emoji_helper(selected_user, df)
+        st.title("Emoji Analysis")
 
+        col1, col2=st.columns(2)
+
+        with col1:
+            st.dataframe(emoji_df)
+        with col2:
+            fig,ax=plt.subplots()
+            ax.pie(emoji_df[1],labels=emoji_df[0],autopct="%0.2f")
+            st.pyplot(fig)
 
