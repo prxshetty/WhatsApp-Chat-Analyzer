4cd436a68e3b4169d61ed5a538dac98c37765f49
Pranam Shetty <pranamshetty@Pranams-MacBook-Pro.local>
2023-12-26 23:23:55 -0500

    theme bug fix code

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
 
-echo "\[theme]
-primaryColor = '#575fe8'
-backgroundColor = '#f5f7f3'
-secondaryBackgroundColor = '#c9eab8'
-textColor = '#262730'
-font = 'sans serif'
+echo "[theme]\n\
+primaryColor = '#575fe8'\n\
+backgroundColor = '#f5f7f3'\n\
+secondaryBackgroundColor = '#c9eab8'\n\
+textColor = '#262730'\n\
+font = 'sans serif'\n\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
