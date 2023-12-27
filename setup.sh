eb1726e02088df33951ab863ad1571f32cb3bfe5
Pranam Shetty <pranamshetty@Pranams-MacBook-Pro.local>
2023-12-26 23:17:13 -0500

    custom theme code

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 mkdir -p ~/.streamlit/
 
-echo "\
+echo "\[theme]
+primaryColor = '#575fe8'
+backgroundColor = '#f5f7f3'
+secondaryBackgroundColor = '#c9eab8'
+textColor = '#262730'
+font = 'sans serif'
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
+
+
